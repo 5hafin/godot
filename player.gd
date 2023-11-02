92a46c59659d508aa121a77ef8641821537e2b4f
5hafin <Sky-Shift@yandex.ru>
2023-11-02 16:45:53 +0700

    Revert "write script for character"
    This reverts commit 33bcfbf97e7dfb2e9591a3d5840f9cb441a5757b.

--- player.gd
+++ player.gd
@@ -1,10 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 500.0
+const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
-@onready var imagePlayer = get_node("player")
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -16,19 +14,15 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("player jump") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("player left", "player right")
+	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	var vel = move_and_slide()
-	if velocity.x<0:
-		imagePlayer.flip_h = true
-	elif velocity.x>0:
-		imagePlayer.flip_h = false
+	move_and_slide()
